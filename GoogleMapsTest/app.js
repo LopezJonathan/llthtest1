79db10a56d8ef1b23dc6e4b5123f9aebf38acaf8
HaakonJ <haakon@haakonjakobson.com>
2017-07-10 22:52:40 -0700

    google map with geolocation
    this is using my api key so you may have to input your own.

---
+++ GoogleMapsTest/app.js
@@ -0,0 +1,15 @@
+(function() {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyC42UIRETC_VVO7cXGqo9ru8TlfDi_-El8",
+        authDomain: "test-project-4d8c5.firebaseapp.com",
+        databaseURL: "https://test-project-4d8c5.firebaseio.com",
+        projectId: "test-project-4d8c5",
+        storageBucket: "test-project-4d8c5.appspot.com",
+        messagingSenderId: "591336136232"
+    };
+    firebase.initializeApp(config);
+
+
+}());

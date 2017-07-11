4ea937dd3335e8e6b305519a7742d3744e4d3ed6
HaakonJ <haakon@haakonjakobson.com>
2017-07-10 23:27:20 -0700

    change original lattitude and longitude

--- GoogleMapsTest/app.js
+++ GoogleMapsTest/app.js
@@ -1,15 +1,45 @@
 (function() {
 
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyC42UIRETC_VVO7cXGqo9ru8TlfDi_-El8",
-        authDomain: "test-project-4d8c5.firebaseapp.com",
-        databaseURL: "https://test-project-4d8c5.firebaseio.com",
-        projectId: "test-project-4d8c5",
-        storageBucket: "test-project-4d8c5.appspot.com",
-        messagingSenderId: "591336136232"
-    };
-    firebase.initializeApp(config);
+    const map = document.getElementById('map');
+    const infoWindow = document.getElementById('infowindow');  
 
+    initMap.on('load', snap => {    
+        map = new google.maps.Map(document.getElementById('map'), {     
+            center: {
+                lat: 47.82128,
+                lng: -122.31559
+            },
+                 zoom: 6    
+        });    
+        infoWindow = new google.maps.InfoWindow;
 
+             // Try HTML5 geolocation.
+            
+        if (navigator.geolocation) {     
+            navigator.geolocation.getCurrentPosition(function(position) {      
+                var pos = {       
+                    lat: position.coords.latitude,
+                           lng: position.coords.longitude      
+                };
+
+                      
+                infoWindow.setPosition(pos);      
+                infoWindow.setContent('Location found.');      
+                infoWindow.open(map);      
+                map.setCenter(pos);     
+            }, function() {      
+                handleLocationError(true, infoWindow, map.getCenter());     
+            });    
+        } else {      // Browser doesn't support Geolocation
+                 
+            handleLocationError(false, infoWindow, map.getCenter());    
+        }   
+    });
+
+       
+    //handleLocationError(browserHasGeolocation, infoWindow, pos) {    
+    //    infoWindow.setPosition(pos);    
+    //    infoWindow.setContent(browserHasGeolocation ?                'Error: The Geolocation service failed.' :                'Error: Your browser doesn\'t support geolocation.');    
+    //    infoWindow.open(map);   
+    //}
 }());

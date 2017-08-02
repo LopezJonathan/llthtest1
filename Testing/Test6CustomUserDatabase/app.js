4c4a8c2225401c225be0674d5ec231a56d499a3a
HaakonJ <haakon@haakonjakobson.com>
2017-08-02 00:04:20 -0700

    updates I did to make push work

--- Testing/Test6CustomUserDatabase/app.js
+++ Testing/Test6CustomUserDatabase/app.js
@@ -1,12 +1,7 @@
+src = "https://www.gstatic.com/firebasejs/4.2.0/firebase.js";
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyC42UIRETC_VVO7cXGqo9ru8TlfDi_-El8",
-    authDomain: "test-project-4d8c5.firebaseapp.com",
-    databaseURL: "https://test-project-4d8c5.firebaseio.com",
-    projectId: "test-project-4d8c5",
-    storageBucket: "test-project-4d8c5.appspot.com",
-    messagingSenderId: "591336136232"
-};
+ 
+var config = {   apiKey: "AIzaSyC42UIRETC_VVO7cXGqo9ru8TlfDi_-El8",   authDomain: "test-project-4d8c5.firebaseapp.com",   databaseURL: "https://test-project-4d8c5.firebaseio.com",   projectId: "test-project-4d8c5",   storageBucket: "test-project-4d8c5.appspot.com",   messagingSenderId: "591336136232"  }; 
 firebase.initializeApp(config);
 
 //Get elements
@@ -14,8 +9,8 @@ const preObject = document.getElementById('object');
 const ulList = document.getElementById('list');
 
 // Create reference
-const dbRefObject = firebase.database().ref().child('object');
-const dbRefList = dbRefObject.child('hobbies').child('egg');
+const dbRefObject = firebase.database().ref();
+const dbRefList = dbRefObject.child('object').child('hobbies').child('egg');
 
 // Sync Object Changes
 dbRefObject.on('value', snap => {
@@ -52,8 +47,6 @@ const txtLogin = document.getElementById('btnLogin');
 const txtSignUp = document.getElementById('btnSignUp');
 const txtLogout = document.getElementById('btnLogout');
 
-const map2 = document.getElementById('map2');
-const LocateButton = document.getElementById('LocateButton');
 const LoginMessage = document.getElementById('LoginMessage');
 
 //Add Login Event
@@ -91,16 +84,30 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
         btnLogout.classList.remove('hide');
         btnLogin.classList.add('hide');
         btnSignUp.classList.add('hide');
-        map2.classList.remove('hide');
-        LocateButton.classList.remove('hide');
         LoginMessage.classList.add('hide');
     } else {
         console.log('not Logged in');
         btnLogout.classList.add('hide');
         btnLogin.classList.remove('hide');
         btnSignUp.classList.remove('hide');
-        map2.classList.add('hide');
-        LocateButton.classList.add('hide');
         LoginMessage.classList.remove('hide');
     }
 });
+
+const name = document.getElementById('name');
+const email2 = document.getElementById('email2');
+const btnSend = document.getElementById('btnSend');
+
+btnSend.addEventListener('click', e => {
+    const dbUserRef = firebase.database().ref();
+    const nameVal = name.value;
+    const emailVal = email.value;
+    const packet = { nameVal, emailVal, timeStamp: new Date().toString(), Lat: 'Lat', Long: 'Long' };
+    const nkey = dbUserRef.child('users').push().getKey();
+    dbUserRef.child('users').child(nkey.toString()).set(packet);
+    console.log("  key: " + nkey);  
+});
+
+//To find an element with random parent
+//firebase.database().ref(`/users/${usersId}`)
+//.child('goingNumber').set(goingNumber )

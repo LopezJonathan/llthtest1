b2a1bcdf7dd2420b645eca6221e63fb06407c7e6
Jonathan Lopez <JonL_2086@hotmail.com>
2017-08-19 23:12:48 -0700

    Restructured the files to redeploy site on firebase
    File structre and configuration files needed to be updated/fixed.

---
+++ public/js/collection.js
@@ -0,0 +1,208 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBi8HNIbweZnYK0RE3oEt0gjj9zpnV9TJo",
+	authDomain: "llth-firebasetest.firebaseapp.com",
+	databaseURL: "https://llth-firebasetest.firebaseio.com",
+	projectId: "llth-firebasetest",
+	storageBucket: "llth-firebasetest.appspot.com",
+	messagingSenderId: "212300061606"
+};
+firebase.initializeApp(config);
+
+// Create reference
+const dbRefObject = firebase.database().ref();
+const dbRefList = dbRefObject.child('stuff');
+
+//Get elements
+const txtName = document.getElementById('txtName');
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const txtLogin = document.getElementById('btnLogin');
+const txtSignUp = document.getElementById('btnSignUp');
+const txtLogout = document.getElementById('btnLogout');
+const LoginMessage = document.getElementById('LoginMessage')
+
+//Add Login Event
+btnLogin.addEventListener('click', e => {
+    //Get email and pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    //Sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+});
+
+//Add signup event
+btnSignUp.addEventListener('click', e => {
+    //Get email and pass
+    //TODO: Check for real Emails
+
+    const name = txtName.value;
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    //Sign in
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+
+    const dbUserRef = firebase.database().ref();
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            dbUserRef.child('users').child(firebaseUser.uid).set({
+                UserName: name,
+                Email: email,
+                timeStamp: new Date().toString(),
+                eui: {
+                    e1ui: { bool: false, eggNum: 1 },
+                    e2ui: { bool: false, eggNum: 2 },
+                    e3ui: { bool: false, eggNum: 3 },
+                    e4ui: { bool: false, eggNum: 4 },
+                    e5ui: { bool: false, eggNum: 5 },
+                    e6ui: { bool: false, eggNum: 6 },
+                    e7ui: { bool: false, eggNum: 7 },
+                    e8ui: { bool: false, eggNum: 8 },
+                    e9ui: { bool: false, eggNum: 9 },
+                    e10ui: { bool: false, eggNum: 10 },
+                    e11ui: { bool: false, eggNum: 11 },
+                    e12ui: { bool: false, eggNum: 12 },
+                    e13ui: { bool: false, eggNum: 13 },
+                    e14ui: { bool: false, eggNum: 14 },
+                    e15ui: { bool: false, eggNum: 15 },
+                    e16ui: { bool: false, eggNum: 16 },
+                    e17ui: { bool: false, eggNum: 17 },
+                    e18ui: { bool: false, eggNum: 18 },
+                    e19ui: { bool: false, eggNum: 19 },
+                    e20ui: { bool: false, eggNum: 20 },
+                    e21ui: { bool: false, eggNum: 21 },
+                    e22ui: { bool: false, eggNum: 22 },
+                    e23ui: { bool: false, eggNum: 23 },
+                    e24ui: { bool: false, eggNum: 24 },
+                    e25ui: { bool: false, eggNum: 25 },
+                    e26ui: { bool: false, eggNum: 26 },
+                    e27ui: { bool: false, eggNum: 27 },
+                    e28ui: { bool: false, eggNum: 28 },
+                    e29ui: { bool: false, eggNum: 29 },
+                    e30ui: { bool: false, eggNum: 30 },
+                    e31ui: { bool: false, eggNum: 31 },
+                    e32ui: { bool: false, eggNum: 32 },
+                    e33ui: { bool: false, eggNum: 33 },
+                    e34ui: { bool: false, eggNum: 34 },
+                    e35ui: { bool: false, eggNum: 35 },
+                    e36ui: { bool: false, eggNum: 36 },
+                    e37ui: { bool: false, eggNum: 37 },
+                    e38ui: { bool: false, eggNum: 38 },
+                    e39ui: { bool: false, eggNum: 39 },
+                    e40ui: { bool: false, eggNum: 40 },
+                    e41ui: { bool: false, eggNum: 41 },
+                    e42ui: { bool: false, eggNum: 42 },
+                    e43ui: { bool: false, eggNum: 43 },
+                    e44ui: { bool: false, eggNum: 44 },
+                    e45ui: { bool: false, eggNum: 45 },
+                    e46ui: { bool: false, eggNum: 46 },
+                    e47ui: { bool: false, eggNum: 47 },
+                    e48ui: { bool: false, eggNum: 48 },
+                    e49ui: { bool: false, eggNum: 49 },
+                    e50ui: { bool: false, eggNum: 50 },
+                    e101ui: { bool: false, eggNum: 101 },
+                    e102ui: { bool: false, eggNum: 102 }
+                }
+            });
+        }
+    });
+});
+
+//
+btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+});
+
+//Add a realtime Listner
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        console.log(firebaseUser);
+        LoginMessage.classList.add('hide');
+        btnLogout.classList.remove('hide');
+        btnLogin.classList.add('hide');
+        btnSignUp.classList.add('hide');
+    } else {
+        console.log('not Logged in');
+        LoginMessage.classList.remove('hide');
+        btnLogout.classList.add('hide');
+        btnLogin.classList.remove('hide');
+        btnSignUp.classList.remove('hide');
+    }
+});
+
+//egg collection function
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        const collection = document.getElementById('collection')
+
+        //const e1 = document.getElementById('e1')
+        const dbUserRef = firebase.database().ref();
+        const dbEggRef = dbUserRef.child('users').child(firebaseUser.uid).child('eui');
+
+        //const notCollected = 0;
+        //const collected = 0;
+
+        dbEggRef.orderByChild('eggNum').on('child_added', function(snap) {
+            if (snap.val().bool == "true") {
+                const section = document.createElement('section');
+                section.classList.add('eggItem');
+                collection.appendChild(section);
+
+                const h1 = document.createElement('h1');
+                h1.innerText = 'Egg ' + snap.val().eggNum;
+                section.appendChild(h1);
+
+                const img = document.createElement('img');
+                const linkText = document.createTextNode('egg' + snap.val().eggNum);
+                img.appendChild(linkText);
+                img.classList.add('eggImage');
+                img.title = 'egg' + snap.val().eggNum;
+                img.id = 'egg' + snap.val().eggNum;
+
+                ////until more images are added this is here so that if any eggs that are listed as greater or less than 101 to 103 have an image
+                ////when new images are added all you will need to do is save an image with the number of the egg and a string value that is static and it will pick it up.
+                //img.src = 'images/TestEgg' + snap.val().eggNum + '.png';
+                if (snap.val().eggNum <= 100 || snap.val().eggNum >= 104) {
+                    img.src = 'images/TestEgg102.png';
+                } else {
+                    img.src = 'images/TestEgg' + snap.val().eggNum + '.png';
+                }
+                ////similarly to the image if the name of a website contains the egg value then it should be able to be pulled up this way.
+                //img.href = 'https://haakonj.github.io/Prototype-Firebase-App/egg' + snap.val().eggNum + '.html'
+
+                section.appendChild(img)
+                    //collected++;
+            } else {
+                const section = document.createElement('section');
+                section.classList.add('eggItem');
+                collection.appendChild(section);
+
+                const h1 = document.createElement('h1');
+                h1.innerText = 'Egg ' + snap.val().eggNum;
+                section.appendChild(h1);
+
+                const img = document.createElement('img');
+                const linkText = document.createTextNode('egg' + snap.val().eggNum);
+                img.appendChild(linkText);
+                img.classList.add('eggImage');
+                img.title = 'egg' + snap.val().eggNum;
+                img.id = 'egg' + snap.val().eggNum;
+                img.src = 'images/NewMysteryEgg.png';
+
+                section.appendChild(img);
+                //notCollected++;
+                //collected++;
+            }
+            //const pecentCollect;
+            //const collectedegg = notCollected / collected;
+            // pecentCollect = Math.round(collectedegg * 100);
+        });
+
+    }
+
+});
